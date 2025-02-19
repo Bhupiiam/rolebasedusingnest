163446c7d090c7a38aa8476d54c039c969be7c84
Bhupiiam <97390120+Bhupiiam@users.noreply.github.com>
2025-02-20 01:14:12 +0530

    Update user.controller.ts

--- src/tasks/dto/user.controller.ts
+++ src/tasks/dto/user.controller.ts
@@ -21,8 +21,8 @@ import { User } from 'src/auth/user.entity';
 import { update_self_dto } from './update_self_dto';
 import { Logs } from '../logs.entity';
 import { AuthService } from 'src/auth/auth.service';
-
-@Controller('user') 
+// now it will accept all the main page request directly without using user in the url
+@Controller('/') 
 export class TasksController {
   constructor(private userService: UserService) {}
 
